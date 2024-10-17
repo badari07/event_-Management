fcab1ec0adba035285efa6c4a002183d0448f548
badari07 <badari0793@gmail.com>
2024-10-18 03:38:41 +0530

    update

--- src/main/java/com/example/event_management/security/SecurityConfig.java
+++ src/main/java/com/example/event_management/security/SecurityConfig.java
@@ -1,36 +1,7 @@
 package com.example.event_management.security;
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    private static final String[] AUTH_ALLOWLIST ={"/", "/public"};
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for development
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .anyRequest().permitAll());
-//                       // .requestMatchers(AUTH_ALLOWLIST).permitAll());
-////                        .anyRequest().authenticated()); // Secure all other requests
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public BCryptPasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//}
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
